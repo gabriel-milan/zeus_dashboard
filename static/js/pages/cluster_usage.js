359e2731830763664a2a478231f5d6baab244099
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2019-10-22 13:37:34 -0300

    Add usage parser

--- static/js/pages/cluster_usage.js
+++ static/js/pages/cluster_usage.js
@@ -1,47 +1,134 @@
 $(document).ready(function () {
 
-    const memoryDiv = document.getElementById('memoryDiv');
-
-    const source = new EventSource("/memory", {withCredentials: true});
-
+    Chart.pluginService.register({
+          beforeDraw: function (chart) {
+              if (chart.config.options.elements.center) {
+          //Get ctx from string
+          var ctx = chart.chart.ctx;
+          
+                  //Get options from the center object in options
+          var centerConfig = chart.config.options.elements.center;
+            var fontStyle = centerConfig.fontStyle || 'Arial';
+                  var txt = centerConfig.text;
+          var color = centerConfig.color || '#000';
+          var sidePadding = centerConfig.sidePadding || 20;
+          var sidePaddingCalculated = (sidePadding/100) * (chart.innerRadius * 2)
+          //Start with a base font of 30px
+          ctx.font = "30px " + fontStyle;
+          
+                  //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+          var stringWidth = ctx.measureText(txt).width;
+          var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+  
+          // Find out how much the font can grow in width.
+          var widthRatio = elementWidth / stringWidth;
+          var newFontSize = Math.floor(30 * widthRatio);
+          var elementHeight = (chart.innerRadius * 2);
+  
+          // Pick a new font size so it will not be larger than the height of label.
+          var fontSizeToUse = Math.min(newFontSize, elementHeight);
+  
+                  //Set font settings to draw it correctly.
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'middle';
+          var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+          var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+          ctx.font = fontSizeToUse+"px " + fontStyle;
+          ctx.fillStyle = color;
+          
+          //Draw text in center
+          ctx.fillText(txt, centerX, centerY);
+              }
+          }
+      });
+  
+    var configMem = {
+      type: 'doughnut',
+      data: {
+        labels: [
+          "Memory Usage",
+          "Free memory"
+        ],
+        datasets: [{
+          data: [],
+          backgroundColor: [
+            "#009b94",
+            "rgba(0,0,0,0)",
+          ],
+          hoverBackgroundColor: [
+            "#009b94",
+            "rgba(0,0,0,0)",
+          ]
+        }],
+      },
+      options: {
+        legend: {
+          display: false,
+        },
+        elements: {
+          center: {
+            text: 'Mem',
+            color: '#eb921f', // Default is #000000
+            fontStyle: 'Arial', // Default is Arial
+            sidePadding: 20 // Defualt is 20 (as a percentage)
+          }
+        },
+      }
+    };
+  
+    var configCPU = {
+      type: 'doughnut',
+      data: {
+        labels: [
+          "CPU Usage",
+          "Free CPU"
+        ],
+        datasets: [{
+          data: [],
+          backgroundColor: [
+            "#009b94",
+            "rgba(0,0,0,0)",
+          ],
+          hoverBackgroundColor: [
+            "#009b94",
+            "rgba(0,0,0,0)",
+          ]
+        }],
+      },
+      options: {
+        legend: {
+          display: false,
+        },
+        elements: {
+          center: {
+            text: 'CPU',
+            color: '#eb921f', // Default is #000000
+            fontStyle: 'Arial', // Default is Arial
+            sidePadding: 20 // Defualt is 20 (as a percentage)
+          }
+        },
+      }
+    };
+  
+    var contextCPU = document.getElementById("cpuUsage").getContext("2d");
+    var contextMem = document.getElementById("memUsage").getContext("2d");
+  
+    var chartCPU = new Chart(contextCPU, configCPU);
+    var chartMem = new Chart(contextMem, configMem);
+  
+    const source = new EventSource("http://localhost:5000/usage", {withCredentials: true});
+  
     source.onmessage = function (event) {
-        
-        // Parsing data
-        const data = JSON.parse(event.data);
-        console.log(data);
-        const keys = [];
-        for (var k in data)
-            keys.push(k);
-        
-        console.log(keys);
-
-        // Building string
-        var textMemory = "";
-
-        function humanFileSize(bytes, si) {
-            var thresh = si ? 1000 : 1024;
-            if(Math.abs(bytes) < thresh) {
-                return bytes + ' B';
-            }
-            var units = si
-                ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
-                : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
-            var u = -1;
-            do {
-                bytes /= thresh;
-                ++u;
-            } while(Math.abs(bytes) >= thresh && u < units.length - 1);
-            return bytes.toFixed(1)+' '+units[u];
-        }        
-
-        // Appending memory
-        for (var i = 0; i < keys.length; i++) {
-            textMemory += "<p><h5>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i style='color: #0073b7;' class='fa fa-check'></i> ";
-            textMemory += keys[i] + ": " + humanFileSize(data[keys[i]], true);
-            textMemory += " </h5></p>";
-        }
-
-        // Setting text
-        memoryDiv.innerHTML = textMemory;
+      // Parsing data
+      const data = JSON.parse(event.data);
+  
+      configCPU.data.datasets[0].data = [data.cpu, 100 - data.cpu];
+      configMem.data.datasets[0].data = [data.mem, 100 - data.mem];
+  
+      chartCPU.update();
+      chartMem.update();
+      
     }
-});
+  
+  });
+  
